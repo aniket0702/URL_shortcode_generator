6a584ff0a363786db03662765bbfebd0e0887e16
aniket0702 <aniketagarwal11133@yahoo.com>
2017-08-24 22:37:25 +0530

    URL validation

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,4 +1,18 @@
 from django import forms
-
+from django.core.validators import URLValidator
 class SubmitUrlForm(forms.Form):
     url = forms.CharField(label = 'Submit Url')
+
+    def clean(self):
+        cleaned_data = super(SubmitUrlForm,self).clean()
+        # url = cleaned_data['url']
+        # print(url)
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        print(url)
+        url_validator = URLValidator()
+        try:
+            url_validator(url)
+        except:
+            raise forms.ValidationError("Invalid URL ")
+        return url
