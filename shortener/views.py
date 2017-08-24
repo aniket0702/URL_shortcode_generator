3691a2ae9fa848babe20998a9148a09656f0192b
aniket0702 <aniketagarwal11133@yahoo.com>
2017-08-24 18:28:51 +0530

    forms

--- shortener/views.py
+++ shortener/views.py
@@ -4,11 +4,23 @@ from django.http import HttpResponse,HttpResponseRedirect,Http404
 from django.views import View
 from django.shortcuts import render,get_object_or_404
 from .models import KirrURL
+from .forms import SubmitUrlForm
 # Create your views here.
 class HomeView(View):
     def get(self,request,*args,**kwargs):
-        return render(request,'shortener/home.html',{})
-
+        form = SubmitUrlForm()
+        context = {
+            "form":form
+        }
+        return render(request,'shortener/home.html',context)
+    def post(self,request,*args,**kwargs):
+        form = SubmitUrlForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['url'])
+        context = {
+            "form":form
+        }
+        return render(request,'shortener/home.html',context)
 class KirrCBView(View):
     def get(self,request,*args,**kwargs):
         obj = get_object_or_404(KirrURL,shortcode = shortcode)
