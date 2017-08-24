6b04d549ed6662a24ee3cfa2193b752746eb423d
aniket0702 <aniketagarwal11133@yahoo.com>
2017-08-24 23:28:55 +0530

    Form validation

--- shortener/forms.py
+++ shortener/forms.py
@@ -5,14 +5,30 @@ class SubmitUrlForm(forms.Form):
 
     def clean(self):
         cleaned_data = super(SubmitUrlForm,self).clean()
-        # url = cleaned_data['url']
-        # print(url)
-    def clean_url(self):
-        url = self.cleaned_data['url']
-        print(url)
+        value_1_invalid = False
+        value_2_invalid = False
+        url = cleaned_data.get('url')
+        print("maiofmoaid")
         url_validator = URLValidator()
         try:
             url_validator(url)
         except:
-            raise forms.ValidationError("Invalid URL ")
-        return url
+            value_1_invalid = True
+        value_2_url = "http://"+url
+        try:
+            url_validator(url)
+        except:
+            value_2_invalid = True
+
+        if not ".com" in url and not value_1_invalid and not value_2_invalid:
+            raise forms.ValidationError("No .com")
+        return cleaned_data
+
+    # def clean_url(self):
+    #     url = self.cleaned_data['url']
+    #     url_validator = URLValidator()
+    #     try:
+    #         url_validator(url)
+    #     except:
+    #         raise forms.ValidationError("Invalid URL ")
+        # return url
--- shortener/views.py
+++ shortener/views.py
@@ -16,7 +16,7 @@ class HomeView(View):
     def post(self,request,*args,**kwargs):
         form = SubmitUrlForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['url'])
+            print(form.cleaned_data.get('url'))
         context = {
             "form":form
         }
